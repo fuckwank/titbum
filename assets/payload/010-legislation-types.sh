715c5e39b41c1c01b1e17bc633a3272be8757f48
Pete Birley <petebirley@gmail.com>
2015-02-25 12:01:23 +0000

    Update 010-legislation-types.sh

--- assets/payload/010-legislation-types.sh
+++ assets/payload/010-legislation-types.sh
@@ -14,3 +14,16 @@ do
    echo "ConnectedGovernment: $Current_legislation_type_name: Created project"
 done
 
+
+mkdir -p /ConnectedGovernment/Gitlab
+gitlab groups > /ConnectedGovernment/Gitlab/010-legislation-types
+
+cat /ConnectedGovernment/Raw/010-legislation-types.csv | while read line
+do
+   echo "$line" > /tmp/Current-legislation-type
+   Current_legislation_type_url=$(awk -F"," '{print $1}' /tmp/Current-legislation-type)
+   Current_legislation_type_name=$(awk -F"," '{print $2}' /tmp/Current-legislation-type)
+   echo "ConnectedGovernment: $Current_legislation_type_name: Getting ID"
+   Current_legislation_type_id=$(cat /ConnectedGovernment/Gitlab/010-legislation-types | grep -E "(^|\s)$(echo "$Current_legislation_type_name" | tr ' ' '_')($|\s)" | awk '{print $3;}')
+   echo "ConnectedGovernment: $Current_legislation_type_name: ID=$Current_legislation_type_id"
+done
