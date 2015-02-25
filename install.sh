989a63015071b2aeeec63913de5de7dd4271ebc4
Pete Birley <petebirley@gmail.com>
2015-02-25 09:37:31 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+apt-get update
+apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
+apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
+rvm install 2.1.1
+rvm use 2.1.1 --default
+ruby -v
