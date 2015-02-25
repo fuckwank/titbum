9854557e2f94d94c8963d7a38ce8e4e530ffe14b
Pete Birley <petebirley@gmail.com>
2015-02-25 09:42:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 apt-get update
 apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
 apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
-rvm install 2.1.1
-rvm use 2.1.1 --default
+source /etc/profile.d/rvm.sh
+rvm install 2.1.5
+rvm use 2.1.5 --default
 ruby -v
