2d1102f34f866fa9f0b1d0ec23d5f917773cca50
Pete Birley <petebirley@gmail.com>
2015-02-25 10:17:19 +0000

    Update 010-legislation-types.sh

--- assets/payload/010-legislation-types.sh
+++ assets/payload/010-legislation-types.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python 010-legislation-types.py > 010-legislation-types.csv
-sed -i '/#browse/d' 010-legislation-types.csv
-cat 010-legislation-types.csv 
+mkdir -p /ConnectedGovernment/Raw
+python 010-legislation-types.py > /ConnectedGovernment/Raw/010-legislation-types.csv
+sed -i '/#browse/d' /ConnectedGovernment/Raw/010-legislation-types.csv
+cat /ConnectedGovernment/Raw/010-legislation-types.csv 
