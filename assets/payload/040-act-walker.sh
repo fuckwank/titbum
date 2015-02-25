0512c4aa6085cae403e1d2a9cc58ce32e42b4542
Pete Birley <petebirley@gmail.com>
2015-02-25 13:11:15 +0000

    Update 040-act-walker.sh

--- assets/payload/040-act-walker.sh
+++ assets/payload/040-act-walker.sh
@@ -4,6 +4,7 @@ set -e
 
 OutputRootDir="/ConnectedGovernment/Raw"
 
+mkdir -p $OutputRootDir
 
 cat /ConnectedGovernment/Gitlab/010-legislation-types | while read line
 do
@@ -11,63 +12,58 @@ do
   Current_legislation_type_id=$(echo $line | awk '{print $2;}')
   Current_legislation_type_slug=$(echo $line | awk '{print $3;}')
   echo $Current_legislation_type_slug $Current_legislation_type_id $Current_legislation_type_name_safe
-  echo "next!"
+  
+  LegislationType="$Current_legislation_type_slug"
+  LegislationYear="2014"
+  LegislationVolume="24"
+  
+  
+  LegislationTitle=$(python 039-act-name.py $LegislationType $LegislationYear $LegislationVolume | tr ' ' '_')
+  
+  if [ "$LegislationTitle" != "404_error" ]; then
+    
+    echo "ConnectecdGovernment: $LegislationTitle found"
+    LegislationOutputDir="$OutputRootDir/$LegislationType/$LegislationTitle"
+  
+    echo "ConnectecdGovernment: $LegislationTitle: Making output directory at $LegislationOutputDir"
+    mkdir -p $LegislationOutputDir
+    
+    LegislationSection="introduction"
+    echo "ConnectecdGovernment: $LegislationTitle: Getting legislation $LegislationSection"
+    python 040-act.py $LegislationType $LegislationYear $LegislationVolume 0 | html2text > $LegislationOutputDir/$LegislationSection.md
+    if [ "$(cat $LegislationOutputDir/$LegislationSection.md | awk '{print $1}')" != "404" ]; then
+      echo "ConnectecdGovernment: $LegislationTitle: Got legislation $LegislationSection, saved at $LegislationOutputDir/$LegislationSection.md"
+    else
+      echo "ConnectecdGovernment: $LegislationTitle: No legislation $LegislationSection, cleaning up repo"
+      rm -f $LegislationOutputDir/$LegislationSection.md
+    fi
+    
+    LegislationSection="body"
+    echo "ConnectecdGovernment: $LegislationTitle: Getting legislation $LegislationSection"
+    python 040-act.py $LegislationType $LegislationYear $LegislationVolume 1 | html2text > $LegislationOutputDir/$LegislationSection.md
+    if [ "$(cat $LegislationOutputDir/$LegislationSection.md | awk '{print $1}')" != "404" ]; then
+      echo "ConnectecdGovernment: $LegislationTitle: Got legislation $LegislationSection, saved at $LegislationOutputDir/$LegislationSection.md"
+    else
+      echo "ConnectecdGovernment: $LegislationTitle: No legislation $LegislationSection, cleaning up repo"
+      rm -f $LegislationOutputDir/$LegislationSection.md
+    fi
+    
+    LegislationSection="schedules"
+    echo "ConnectecdGovernment: $LegislationTitle: Getting legislation $LegislationSection"
+    python 040-act.py $LegislationType $LegislationYear $LegislationVolume 2 | html2text > $LegislationOutputDir/$LegislationSection.md
+    if [ "$(cat $LegislationOutputDir/$LegislationSection.md | awk '{print $1}')" != "404" ]; then
+      echo "ConnectecdGovernment: $LegislationTitle: Got legislation $LegislationSection, saved at $LegislationOutputDir/$LegislationSection.md"
+    else
+      echo "ConnectecdGovernment: $LegislationTitle: No legislation $LegislationSection, cleaning up repo"
+      rm -f $LegislationOutputDir/$LegislationSection.md
+    fi
+    
+  else
+    echo "ConnectecdGovernment: No legislation found"
+  fi
+  
 done
 
 
 
 
-echo "$Current_legislation_type_url"
-echo "$Current_legislation_type_name"
-echo "$Current_legislation_type_slug"
-
-
-
-LegislationType="ukpga"
-LegislationYear="2014"
-LegislationVolume="24"
-
-
-LegislationTitle=$(python 039-act-name.py $LegislationType $LegislationYear $LegislationVolume | tr ' ' '_')
-
-if [ "$LegislationTitle" != "404_error" ]; then
-  
-  echo "ConnectecdGovernment: $LegislationTitle found"
-  LegislationOutputDir="$OutputRootDir/$LegislationType/$LegislationTitle"
-
-  echo "ConnectecdGovernment: $LegislationTitle: Making output directory at $LegislationOutputDir"
-  mkdir -p $LegislationOutputDir
-  
-  LegislationSection="introduction"
-  echo "ConnectecdGovernment: $LegislationTitle: Getting legislation $LegislationSection"
-  python 040-act.py $LegislationType $LegislationYear $LegislationVolume 0 | html2text > $LegislationOutputDir/$LegislationSection.md
-  if [ "$(cat $LegislationOutputDir/$LegislationSection.md | awk '{print $1}')" != "404" ]; then
-    echo "ConnectecdGovernment: $LegislationTitle: Got legislation $LegislationSection, saved at $LegislationOutputDir/$LegislationSection.md"
-  else
-    echo "ConnectecdGovernment: $LegislationTitle: No legislation $LegislationSection, cleaning up repo"
-    rm -f $LegislationOutputDir/$LegislationSection.md
-  fi
-  
-  LegislationSection="body"
-  echo "ConnectecdGovernment: $LegislationTitle: Getting legislation $LegislationSection"
-  python 040-act.py $LegislationType $LegislationYear $LegislationVolume 1 | html2text > $LegislationOutputDir/$LegislationSection.md
-  if [ "$(cat $LegislationOutputDir/$LegislationSection.md | awk '{print $1}')" != "404" ]; then
-    echo "ConnectecdGovernment: $LegislationTitle: Got legislation $LegislationSection, saved at $LegislationOutputDir/$LegislationSection.md"
-  else
-    echo "ConnectecdGovernment: $LegislationTitle: No legislation $LegislationSection, cleaning up repo"
-    rm -f $LegislationOutputDir/$LegislationSection.md
-  fi
-  
-  LegislationSection="schedules"
-  echo "ConnectecdGovernment: $LegislationTitle: Getting legislation $LegislationSection"
-  python 040-act.py $LegislationType $LegislationYear $LegislationVolume 2 | html2text > $LegislationOutputDir/$LegislationSection.md
-  if [ "$(cat $LegislationOutputDir/$LegislationSection.md | awk '{print $1}')" != "404" ]; then
-    echo "ConnectecdGovernment: $LegislationTitle: Got legislation $LegislationSection, saved at $LegislationOutputDir/$LegislationSection.md"
-  else
-    echo "ConnectecdGovernment: $LegislationTitle: No legislation $LegislationSection, cleaning up repo"
-    rm -f $LegislationOutputDir/$LegislationSection.md
-  fi
-  
-else
-  echo "ConnectecdGovernment: No legislation found"
-fi
