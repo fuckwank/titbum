6c37f4a9133982b8e85ff9255240917538a057b6
Pete Birley <petebirley@gmail.com>
2015-02-25 10:15:49 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,9 @@ export GITLAB_API_KEY=$GITLAB_API_KEY
 sed -i "s/super_secret_key_from_gitlab/$GITLAB_API_KEY/g" /root/.gitlab.yml
 source /etc/profile.d/rvm.sh
 gitlab projects
+
+
+cd /payload && \
+./010-legislation-types.sh
+
 /bin/bash
