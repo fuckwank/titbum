8d0aad647845d06144c7cc71f13c3fbb2500c964
Pete Birley <petebirley@gmail.com>
2015-02-25 11:37:22 +0000

    Update 010-legislation-types.sh

--- assets/payload/010-legislation-types.sh
+++ assets/payload/010-legislation-types.sh
@@ -3,3 +3,14 @@ mkdir -p /ConnectedGovernment/Raw
 python 010-legislation-types.py > /ConnectedGovernment/Raw/010-legislation-types.csv
 sed -i '/#browse/d' /ConnectedGovernment/Raw/010-legislation-types.csv
 cat /ConnectedGovernment/Raw/010-legislation-types.csv 
+
+cat /ConnectedGovernment/Raw/010-legislation-types.csv | while read line
+do
+   echo "$line" > /tmp/Current-legislation-type
+   Current_legislation_type_url=$(awk -F"," '{print $1}' /tmp/Current-legislation-type)
+   Current_legislation_type_name=$(awk -F"," '{print $2}' /tmp/Current-legislation-type)
+   echo "ConnectedGovernment: $Current_legislation_type_name: Createing project"
+   gitlab create_group $(echo "$Current_legislation_type_name" | tr ' ' '_') $(echo "$Current_legislation_type_name" | tr ' ' '_')
+   echo "ConnectedGovernment: $Current_legislation_type_name: Created project"
+done
+
