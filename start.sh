9efbd7da7e8b38618bdfe98069349ba20193e132
Pete Birley <petebirley@gmail.com>
2015-02-25 10:52:12 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 export GITLAB_API_KEY=$GITLAB_API_KEY
 sed -i "s/super_secret_key_from_gitlab/$GITLAB_API_KEY/g" /root/.gitlab.yml
 source /etc/profile.d/rvm.sh
+
+export GITLAB_API_ENDPOINT="http://connectedgovernment.uk/api/v3"
+export GITLAB_API_PRIVATE_TOKEN="$GITLAB_API_KEY"
 gitlab projects
 
 
