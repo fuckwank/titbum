030c1a07b17a0b70f85ec1c2f29057fbe4a5e4f9
Pete Birley <petebirley@gmail.com>
2015-02-25 06:36:30 +0000

    Create 010-legislation-types.py

---
+++ assets/010-legislation-types.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import urllib2
+url="http://www.legislation.gov.uk/browse"
+baseurl="http://www.legislation.gov.uk"
+page=urllib2.urlopen(url)
+soup = BeautifulSoup(page.read())
+
+content=soup.find(id="content")
+Legislation= content.find_all('a')
+
+for LegislationType in Legislation:
+   print baseurl+LegislationType['href']+","+str(LegislationType.string)
