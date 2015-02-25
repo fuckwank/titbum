4faa5f639f328179af4d708e1699323fd64c633c
Pete Birley <petebirley@gmail.com>
2015-02-25 14:10:46 +0000

    Update 010-legislation-types.sh

--- assets/payload/010-legislation-types.sh
+++ assets/payload/010-legislation-types.sh
@@ -6,6 +6,7 @@ python 010-legislation-types.py > /ConnectedGovernment/Raw/010-legislation-types
 sed -i '/#browse/d' /ConnectedGovernment/Raw/010-legislation-types.csv
 cat /ConnectedGovernment/Raw/010-legislation-types.csv 
 
+rm -f /ConnectedGovernment/Gitlab/010-legislation-types
 touch /ConnectedGovernment/Gitlab/010-legislation-types
 cat /ConnectedGovernment/Raw/010-legislation-types.csv | while read line
 do
@@ -15,6 +16,7 @@ do
    Current_legislation_type_name_safe=$(echo "$Current_legislation_type_name" | sed -e 's/^[ \t]*//' | tr ' ' '_')
    echo "ConnectedGovernment: $Current_legislation_type_name: Createing project"
    Current_legislation_type_id=$(gitlab create_group $Current_legislation_type_name_safe $Current_legislation_type_name_safe | grep -E "(^|\s)$(echo "id" | tr ' ' '_')($|\s)" | awk '{print $4;}')
+   gitlab add_group_member "$Current_legislation_type_id" 1 50
    Current_legislation_type_slug=$(echo $Current_legislation_type_url | sed 's/http:\/\/www.legislation.gov.uk\///')
    echo $Current_legislation_type_name_safe $Current_legislation_type_id $Current_legislation_type_slug >> /ConnectedGovernment/Gitlab/010-legislation-types
    echo "ConnectedGovernment: $Current_legislation_type_name: Created project"
